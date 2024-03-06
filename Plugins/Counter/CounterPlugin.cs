d63093c564878e06c31fec719bed8ed2e72cea36
shevi <shevijacob@gmail.com>
2024-03-06 23:24:56 +0200

    add Enter

--- Plugins/Counter/CounterPlugin.cs
+++ Plugins/Counter/CounterPlugin.cs
@@ -12,6 +12,7 @@ namespace Counter
         public PluginOutput Execute(PluginInput input)
         {
 
+
             var lastCount = int.Parse(input.PersistentData);
             var result = (lastCount + 1).ToString();
             return new PluginOutput(result, result);
